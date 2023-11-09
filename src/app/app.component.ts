117e6ff2db4844e5cefb8db5b232de267828e7ca
Ausaf Hussain <CZ0232@cztech.com>
2023-11-09 11:29:55 +0530

    added obs subs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import {
+  MSAL_GUARD_CONFIG,
+  MsalBroadcastService,
+  MsalGuardConfiguration,
+  MsalService,
+} from '@azure/msal-angular';
+import { InteractionStatus } from '@azure/msal-browser';
+import { Subject, filter, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'azadlogin';
-
+  private readonly _destroy = new Subject<void>();
   isUserLoggedIn: boolean = false;
+
+  constructor(
+    @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
+    private msalBroadCastService: MsalBroadcastService,
+    private authService: MsalService
+  ) {}
+
+  ngOnInit(): void {
+    this.msalBroadCastService.inProgress$
+      .pipe(
+        filter(
+          (interactionStatus: InteractionStatus) =>
+            interactionStatus == InteractionStatus.None
+        ),
+        takeUntil(this._destroy)
+      )
+      .subscribe((item) => {
+        this.isUserLoggedIn =
+          this.authService.instance.getAllAccounts().length > 0;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._destroy.next(undefined);
+    this._destroy.complete();
+  }
 
   login() {}
   logout() {}
